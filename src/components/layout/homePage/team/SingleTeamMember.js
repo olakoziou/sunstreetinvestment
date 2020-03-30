0d136667b7ceb153005fe01755f10ca9b677f901
Ola Kozioł <o.stanislawska92@gmail.com>
2020-03-30 19:34:52 +0200

    create single team member component

---
+++ src/components/layout/homePage/team/SingleTeamMember.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors } from '../../../../colors';
+import { boxShadows } from '../../../../mixins';
+
+const DivMember = styled.div`
+  min-width: 15rem;
+  width: 25rem;
+  min-height: 20rem;
+  height: 22rem;
+  margin: 5rem 2rem;
+  background-color: rgba(${colors.secondary5});
+  ${boxShadows('small')};
+  position: relative;
+
+  &:after {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(${colors.primary5});
+    transform: skew(-3deg);
+    z-index: -100;
+  }
+
+  & .single-team-member__image {
+    position: relative;
+    transform: translateY(-6rem);
+  }
+
+  & .single-team-member__image > div {
+    background-position: center;
+    background-size: cover;
+    height: 10rem;
+    width: 10rem;
+    margin: 0 auto;
+    border-radius: 50%;
+    border: 5px solid rgba(${colors.primary5});
+    ${boxShadows('small')};
+  }
+
+  & .single-team-member__content {
+    position: relative;
+    transform: translateY(-6rem);
+    text-align: center;
+    padding: 1.5rem;
+
+    & h5 {
+      margin: 1rem;
+      font-size: 1.6rem;
+
+      & span {
+        color: rgba(${colors.extra}, 1);
+        text-shadow: 0 1px 10px rgba(0, 0, 0, 0.25);
+      }
+    }
+
+    & p {
+      font-size: 1.1rem;
+      padding: 1rem;
+    }
+  }
+`;
+
+function SingleTeamMember(props) {
+  const { name, lastName, description, img } = props;
+  return (
+    <DivMember className="single-team-member">
+      <div className="single-team-member__image">
+        <div className="img" style={{ backgroundImage: `url(${img})` }}></div>
+      </div>
+      <div className="single-team-member__content">
+        <h5>
+          {name} <span>{lastName}</span>
+        </h5>
+        <p>{description}</p>
+      </div>
+    </DivMember>
+  );
+}
+
+export default SingleTeamMember;
