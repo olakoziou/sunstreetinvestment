9cb859ca4d93ccfc46f6598743a6d9d84d94ebf9
Ola Kozioł <o.stanislawska92@gmail.com>
2020-03-30 19:40:44 +0200

    add color variables

---
+++ src/colors.js
@@ -0,0 +1,14 @@
+export const colors = {
+  primary: '32, 64, 81',
+  primary2: '47, 89, 111',
+  primary3: '59, 112, 139',
+  primary4: '68, 128, 159',
+  primary5: '77, 145, 180',
+  secondary: '43, 43, 43',
+  secondary2: '90, 90, 90',
+  secondary3: '130, 130, 130',
+  secondary4: '204, 204, 204',
+  secondary5: '228, 228, 228',
+  secondary6: '241, 242, 243',
+  extra: '190, 124, 107'
+};
