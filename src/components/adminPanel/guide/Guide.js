d8530ed275227ea271a76716a806d3e2ccc486db
Ola Kozioł <o.stanislawska92@gmail.com>
2020-05-13 18:17:22 +0200

    finish guide for team members

--- src/components/adminPanel/guide/Guide.js
+++ src/components/adminPanel/guide/Guide.js
@@ -21,35 +21,82 @@ function Guide() {
           <ul>
             <li>
               1. Jeśli oferta ma być dodana na stronę Archiwum (pierwsze
-              inwestycje SSI), zaznacz checkbox
+              inwestycje SSI), zaznacz checkbox.
             </li>
-            <li>
-              2. Pola Nazwa oferty, Zdjęcie Główne oraz Opiekun oferty są
-              wymagane
-            </li>
+            <li>2. Pola z * są wymagane.</li>
             <li>
               3. Zatwierdzenie oferty nie jest równoznaczne z dodaniem oferty na
-              stronę
+              stronę.
             </li>
           </ul>
         </li>
         <li className="item__main">
-          Dodane nieruchomości<span></span>
+          <span>Dodane nieruchomości</span>
+          <ul>
+            <li>
+              1. Tu znajdują się wszystkie dodane nieruchomości z
+              wyszczególnionymi informacjami. Jeśli nie ma błędów, kliknij
+              DODAJ. Oferta zostanie dodana na stronę.
+            </li>
+            <li>
+              2. Jeżeli zauważysz błąd, EDYTUJ ofertę lub ją USUŃ. Po każdej
+              edycji wróć na stronę główną i ponownie wejdź w link do oferty.
+            </li>{' '}
+            <li>
+              3. Jeśli nieruchomość została sprzedana, zarezerwowana lub została
+              podpisana umowa przedwstępna, kliknij EDYTUJ i zaznacz odpowiednią
+              opcję (Stan)
+            </li>
+          </ul>
         </li>
         <li className="item__main">
           <span>Archiwum</span>
+          <ul>
+            <li>JW</li>
+          </ul>
         </li>
         <li className="item__main">
           <span>Usunięte nieruchomości</span>
+          <ul>
+            <li>
+              1. Tu znajdują się nieruchomości usunięte z zakładek{' '}
+              <span>Dodane nieruchomości</span> oraz <span>Archiwum</span>.
+            </li>
+            <li>2. Możesz je przywrócić lub usunąć na stałe.</li>
+            <li>
+              3. Po przywróceniu znajdą się w odpowiedniej zakładce. Możesz je
+              edytować lub dodać na stronę.
+            </li>
+          </ul>
         </li>
         <li className="item__main">
           <span>Użytkownicy</span>
+          <ul>
+            <li>
+              1. W tej zakładce znajdziesz WSZYSTKICH użytkowników, którzy się
+              zarejestrowali.
+            </li>
+            <li>
+              2. Możesz ich zatwierdzić i dodać na stronę lub usunąć ze strony.
+            </li>
+          </ul>
         </li>
         <li className="item__main">
           <span>Konto</span>
+          <ul>
+            <li>1. W tej zakładce możesz edytować swoje dane.</li>
+            <li>2. Każda edycja wymaga potwierdzenia hasłem</li>
+            <li>
+              3. Jeśli nie pamiętasz hasła, wejdź na stronę logowania i zresetuj
+              hasło
+            </li>
+          </ul>
         </li>
         <li className="item__main">
           <span>Strona główna</span>
+          <ul>
+            <li>1. Przejście na stronę główną bez wylogowania</li>
+          </ul>
         </li>
       </ul>
     </GuideDiv>
